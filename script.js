a576040a8af202c315d263ff274cd9070fd15d2d
RikoHaryadi <rikoharyadi0908@gmail.com>
2024-09-03 13:37:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,30 +18,3 @@ function toggleMenu(menuId) {
     }
 }
 
-// Fungsi untuk membuat elemen salju
-function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-
-    // Ukuran salju secara acak
-    const size = Math.random() * 10 + 5; 
-    snowflake.style.width = `${size}px`;
-    snowflake.style.height = `${size}px`;
-
-    // Posisi horizontal acak
-    snowflake.style.left = `${Math.random() * window.innerWidth}px`;
-
-    // Durasi animasi acak
-    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; // Durasi antara 2s dan 5s
-
-    // Tambahkan salju ke elemen dengan class "snow"
-    document.querySelector('.snow').appendChild(snowflake);
-
-    // Hapus salju setelah animasinya selesai
-    setTimeout(() => {
-        snowflake.remove();
-    }, 5000); // 5000ms = 5s
-}
-
-// Membuat salju baru setiap 100ms
-setInterval(createSnowflake, 100);
